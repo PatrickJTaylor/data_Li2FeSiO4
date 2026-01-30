bad0811ef7e099b178a2e9806991193f22e0cb56
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2026-01-30 15:23:15 +0000

    Add trivial docstring to custom exception

--- src/result_regeneration/errors.py
+++ src/result_regeneration/errors.py
@@ -1,4 +1,9 @@
 class DependencyError(Exception):
+    """
+    A trivial custom exception to explicitly highlight the dependency of certain figure
+    plotting scripts on the pre-calculation of Wannier-assigned MD oxidation states.
+    """
+
     def __init__(self) -> None:
         message = "scripts/assign_md_oxidation_states.py must be run to regenerate Wannier-assigned oxidation states for selected AIMD frames."
 
